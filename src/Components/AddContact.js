562cf5928f29ddab4495e593098cbe05ec7bf64d
Jatin957 <jatinc957@gmail.com>
2025-04-18 15:13:21 +0530

    add the contact list component and place to add name and email

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -4,8 +4,23 @@ class AddContact extends React.Component{
     render() {
         return (
             <div className="ui main">
-                
+                <h2>Add Contact</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <lable>Name</lable>
+                        <input type="text" name="name" placeholder="Name" />
+                    </div>
+
+                    <div className="field">
+                        <lable>Email</lable>
+                        <input type="text" email="email" placeholder="Email" />
+                    </div>
+                     <button className="ui button blue">Add</button>
+
+                </form>
             </div>
         );
     }
 }
+
+export default AddContact
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.js"
-// import AddContact from "./AddContact.js"
-// import ContactList from "./ContactList.js"
+import AddContact from "./AddContact.js"
+import ContactList from "./ContactList.js"
 
 
 function App() {
+
+  const contacts  =[
+    {
+      id : "1",
+      name: "Jatin",
+      email: "jatinc957@gmail.com"
+    },
+    {
+      id : "2",
+      name: "Aanchal",
+      email: "aanchals957@gmail.com"
+    }
+  ] 
   return (
-    <div>
+    <div className="ui container">
     <Header/>
-    {/* /* <AddContact/>
-    <ContactList/> */}
+    <AddContact/>
+    <ContactList contacts={contacts}/> 
     </div>
   );
 }
